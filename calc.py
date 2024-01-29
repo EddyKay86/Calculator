b0f4f6102882f64725d555d34f1f113cbe167d95
EddyKay86 <eddykay707@gmail.com>
2024-01-29 12:58:16 +0000

    made changes and will update soon

--- calc.py
+++ calc.py
@@ -3,33 +3,3 @@ b=int(input("Enter b:"))
 c=int(input("Enter c:"))
 operator=input("Enter Operator:")
 
-def addition(a, b, c):
-    return a+b+c
-
-def subtraction(a,b,c):
-    return a-b-c
-
-def multiple(a,b,c):
-    return a*b*c
-
-def division(a,b,c):
-    return a/b/c
-
-if operator=="+":
-    addition(a,b,c)
-    result=addition(a,b,c)
-    print(result)
-elif operator=="*":
-    multiple(a,b,c)
-    result=multiple(a,b,c)
-    print(result)
-elif operator=="-":
-    subtraction(a,b,c)
-    result=subtraction(a,b,c)
-    print(result)
-elif operator=="/":
-    division(a,b,c)
-    result=division(a,b.c)
-    print(result)
-else:
-    print("Invalid operator")
