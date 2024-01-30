4ac56259dfa86e2801024d0488700ab3d251c1b2
EddyKay86 <eddykay707@gmail.com>
2024-01-30 18:59:01 +0000

    changes made

--- calc.py
+++ calc.py
@@ -5,7 +5,23 @@ def calculator():
         if num.lower() == 'done':
             break
         numbers.append(float(num)) 
-    operation = input("Enter operation (+, -, *, /): ")
+    operation = input("Enter the operation (+, -, *, /): ")
+    
+    if operation == '+':
+        result = sum(numbers)
+    elif operation == '-':
+        result = numbers[0] - sum(numbers[1:])
+    elif operation == '*':
+        result = 1
+        for number in numbers:
+            result *= number
+    elif operation == '/':
+        result = numbers[0]
+        for number in numbers[1:]:
+            result /= number
+    else:
+        result = "Invalid operation"
+
     
     
 
