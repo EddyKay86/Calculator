59b77a6938147e98d9cb8ff1c5718a77914a2ce9
EddyKay86 <eddykay707@gmail.com>
2024-02-05 13:01:01 +0000

    complete

--- calc.py
+++ calc.py
@@ -1,28 +1,33 @@
 def calculator():
     numbers = []
+    
     while True:
         num = input("Enter a number(or 'done' to finish): ")
         if num.lower() == 'done':
             break
         numbers.append(float(num)) 
     operation = input("Enter the operation (+, -, *, /): ")
-    
+
     if operation == '+':
         result = sum(numbers)
+        print(result)
     elif operation == '-':
         result = numbers[0] - sum(numbers[1:])
+        print(result)
     elif operation == '*':
         result = 1
         for number in numbers:
             result *= number
+            print(result)
     elif operation == '/':
         result = numbers[0]
         for number in numbers[1:]:
             result /= number
+            print(result)
     else:
         result = "Invalid operation"
         print("The result is:",result)
-        calculator()
+calculator()
 
     
     
